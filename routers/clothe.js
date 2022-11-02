28f1b5207cc34d2b3433939c51e865d79de413d7
jeun-990806 <kjeun990806@gmail.com>
2022-11-02 12:06:12 +0900

    요청/응답 JSON의 material_code, color_code를 배열로 변경

--- routers/clothe.js
+++ routers/clothe.js
@@ -39,18 +39,12 @@ const fs = require("fs");
  *                type: integer
  *              ex_price:
  *                type: string
- *              color_code_1:
- *                  type: string
- *              color_code_2:
- *                  type: string
+ *              color_code:
+ *                  type: array
  *              purchase_date:
  *                  type: string
- *              material_code_1:
- *                  type: integer
- *              material_code_2:
- *                  type: integer
- *              material_code_3:
- *                  type: integer
+ *              material_code:
+ *                  type: array
  *          encoding:
  *            images:
  *              contentType: image/png, image/jpg, image/jpeg
@@ -69,13 +63,14 @@ router.post(
   session_check,
   image_file_uploader,
   async (request, response) => {
-    clothe_info = request.body;
+    let clothe_info = {};
     clothe_info["user_email"] = request.session.user_email;
-    clothe_info["main_category_id"] = Number(clothe_info["main_category_id"]);
-    clothe_info["sub_category_id"] = Number(clothe_info["sub_category_id"]);
-    clothe_info["price"] = Number(clothe_info["price"]);
-    clothe_info["condition_code"] = Number(clothe_info["condition_code"]);
-    clothe_info["shipping_fee"] = Number(clothe_info["shipping_fee"]);
+    clothe_info["main_category_id"] = Number(request.body["main_category_id"]);
+    clothe_info["sub_category_id"] = Number(request.body["sub_category_id"]);
+    clothe_info["name"] = request.body["name"];
+    clothe_info["price"] = Number(request.body["price"]);
+    clothe_info["condition_code"] = Number(request.body["condition_code"]);
+    clothe_info["shipping_fee"] = Number(request.body["shipping_fee"]);
     var current_date = new Date();
     clothe_info[
       "upload_date"
@@ -83,20 +78,24 @@ router.post(
     clothe_info[
       "upload_time"
     ] = `${current_date.getHours()}:${current_date.getMinutes()}:${current_date.getSeconds()}`;
-    if (clothe_info["brand_id"] !== undefined)
-      clothe_info["brand_id"] = Number(clothe_info["brand_id"]);
-    if (clothe_info["purchase_place_id"] !== undefined)
+    if (request.body["brand_id"] !== undefined)
+      clothe_info["brand_id"] = Number(request.body["brand_id"]);
+    if (request.body["purchase_place_id"] !== undefined)
       clothe_info["purchase_place_id"] = Number(
-        clothe_info["purchase_place_id"]
+        request.body["purchase_place_id"]
       );
-    if (clothe_info["ex_price"] !== undefined)
-      clothe_info["ex_price"] = Number(clothe_info["ex_price"]);
-    if (clothe_info["material_code_1"] !== undefined)
-      clothe_info["material_code_1"] = Number(clothe_info["material_code_1"]);
-    if (clothe_info["material_code_2"] !== undefined)
-      clothe_info["material_code_2"] = Number(clothe_info["material_code_2"]);
-    if (clothe_info["material_code_3"] !== undefined)
-      clothe_info["material_code_3"] = Number(clothe_info["material_code_3"]);
+    if (request.body["ex_price"] !== undefined)
+      clothe_info["ex_price"] = Number(request.body["ex_price"]);
+    if (request.body["color_code"][0] !== undefined)
+      clothe_info["color_code_1"] = request.body["color_code"][0];
+    if (request.body["color_code"][1] !== undefined)
+      clothe_info["color_code_2"] = request.body["color_code"][1];
+    if (request.body["material_code"][0] !== undefined)
+      clothe_info["material_code_1"] = Number(request.body["material_code"][0]);
+    if (request.body["material_code"][1] !== undefined)
+      clothe_info["material_code_2"] = Number(request.body["material_code"][1]);
+    if (request.body["material_code"][2] !== undefined)
+      clothe_info["material_code_3"] = Number(request.body["material_code"][2]);
     const db_pool = await mysql_pool.get_pool();
     db_pool.getConnection((error, connection) => {
       if (error) response.sendStatus(500);
@@ -104,7 +103,7 @@ router.post(
         connection.query(
           tools.dictionary_to_sql("clothes", clothe_info),
           (error, result) => {
-            if (error) response.sendStatus(400);
+            if (error) response.sendStatus(500);
             else {
               var clothe_id = result.insertId;
               var status_code = 200;
@@ -192,24 +191,15 @@ router.post(
  *                ex_price:
  *                  type: integer
  *                  example: 62000
- *                color_code_1:
- *                  type: string
- *                  example: '#0000ff'
- *                color_code_2:
- *                  type: string
- *                  example: null
+ *                color_code:
+ *                  type: array
+ *                  example: ['#0000ff']
  *                purchase_date:
  *                  type: string
  *                  example: null
- *                material_code_1:
- *                  type: integer
- *                  example: 1
- *                material_code_2:
- *                  type: integer
- *                  example: 3
- *                material_code_3:
- *                  type: integer
- *                  example: null
+ *                material_code:
+ *                  type: array
+ *                  example: [1, 3]
  *                description:
  *                  type: string
  *                  example: null
@@ -238,7 +228,7 @@ router.get("/read", async (request, response) => {
         (error, rows) => {
           if (error) response.sendStatus(500);
           else {
-            if (rows.length === 0) response.sendStatus(401);
+            if (rows.length === 0) response.sendStatus(404);
             else {
               const { file_name, file_type, ...result } = rows[0];
               result["images"] = [];
@@ -247,6 +237,20 @@ router.get("/read", async (request, response) => {
                   file_name: file_data.file_name,
                 });
               }
+              result["color_code"] = [
+                result["color_code_1"],
+                result["color_code_2"],
+              ];
+              delete result["color_code_1"];
+              delete result["color_code_2"];
+              result["material_code"] = [
+                result["material_code_1"],
+                result["material_code_2"],
+                result["material_code_3"],
+              ];
+              delete result["material_code_1"];
+              delete result["material_code_2"];
+              delete result["material_code_3"];
               response.status(200);
               response.send(result);
             }
@@ -363,24 +367,15 @@ router.get("/read", async (request, response) => {
  *                  ex_price:
  *                    type: integer
  *                    example: 62000
- *                  color_code_1:
- *                    type: string
- *                    example: '#0000ff'
- *                  color_code_2:
- *                    type: string
- *                    example: null
+ *                  color_code:
+ *                    type: array
+ *                    example: ['#0000ff']
  *                  purchase_date:
  *                    type: string
  *                    example: null
- *                  material_code_1:
- *                    type: integer
- *                    example: 1
- *                  material_code_2:
- *                    type: integer
- *                    example: 3
- *                  material_code_3:
- *                    type: integer
- *                    example: null
+ *                  material_code:
+ *                    type: array
+ *                    example: [1, 3]
  *                  description:
  *                    type: string
  *                    example: null
@@ -533,6 +528,22 @@ router.get("/list", async (request, response) => {
           if (error) response.sendStatus(500);
           else {
             response.status(200);
+            for (var i = 0; i < rows.length; i++) {
+              rows[i]["color_code"] = [
+                rows[i]["color_code_1"],
+                rows[i]["color_code_2"],
+              ];
+              delete rows[i]["color_code_1"];
+              delete rows[i]["color_code_2"];
+              rows[i]["material_code"] = [
+                rows[i]["material_code_1"],
+                rows[i]["material_code_2"],
+                rows[i]["material_code_3"],
+              ];
+              delete rows[i]["material_code_1"];
+              delete rows[i]["material_code_2"];
+              delete rows[i]["material_code_3"];
+            }
             response.send(rows);
           }
         }
@@ -640,18 +651,12 @@ router.delete("/delete", session_check, async (request, response) => {
  *                type: integer
  *              ex_price:
  *                type: string
- *              color_code_1:
- *                  type: string
- *              color_code_2:
- *                  type: string
+ *              color_code:
+ *                type: array
  *              purchase_date:
- *                  type: string
- *              material_code_1:
- *                  type: integer
- *              material_code_2:
- *                  type: integer
- *              material_code_3:
- *                  type: integer
+ *                type: string
+ *              material_code:
+ *                type: array
  *
  *    responses:
  *      200:
@@ -668,6 +673,26 @@ router.put("/update", session_check, async (request, response) => {
   const target_id = request.query.clothe_id;
   const user_email = request.session.user_email;
 
+  if (request.body.color_code !== undefined) {
+    if (request.body.color_code[0] !== undefined)
+      request.body["color_code_1"] = request.body.color_code[0];
+    if (request.body.color_code[1] !== undefined)
+      request.body["color_code_2"] = request.body.color_code[1];
+    delete request.body.color_code;
+  }
+
+  if (request.body.material_code !== undefined) {
+    if (request.body.material_code[0] !== undefined)
+      request.body["material_code_1"] = request.body.material_code[0];
+    if (request.body.material_code[1] !== undefined)
+      request.body["material_code_2"] = request.body.material_code[1];
+    if (request.body.material_code[2] !== undefined)
+      request.body["material_code_3"] = request.body.material_code[2];
+    delete request.body.material_code;
+  }
+
+  console.log(request.body);
+
   if (target_id === undefined) {
     response.sendStatus(404);
   } else {
@@ -676,14 +701,14 @@ router.put("/update", session_check, async (request, response) => {
       if (
         field === "name" ||
         field.includes("color_code") ||
-        field.includes("material_code") ||
         field.includes("date")
       ) {
-        update_sql += field + "='" + request.body[field] + "' ";
-      } else update_sql += field + "=" + request.body[field] + " ";
+        update_sql += field + "='" + request.body[field] + "', ";
+      } else update_sql += field + "=" + request.body[field] + ", ";
     }
+    update_sql = update_sql.slice(0, -2);
     update_sql +=
-      "WHERE clothe_id=" + target_id + " AND user_email='" + user_email + "';";
+      " WHERE clothe_id=" + target_id + " AND user_email='" + user_email + "';";
 
     const db_pool = await mysql_pool.get_pool();
     db_pool.getConnection((error, connection) => {
